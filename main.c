a3a568189463a4c439e54088bac7e3a0ff79557e
LuanFabricio <luanlimaleite@hotmail.com>
2024-03-06 12:38:24 -0300

    feat: Now the main example uses blocks generated by raylib/level_editor.c

--- main.c
+++ main.c
@@ -1,15 +1,26 @@
+#include "defines.h"
 #include "mm.h"
 #include "quadtree.h"
 #include "./platform.h"
+#include "types.h"
+#include "utils/levels.h"
 
 int main()
 {
 	QuadTree *root = QuadTree_new((float)HALF_WIDTH, (float)HALF_HEIGHT, (float)HALF_WIDTH);
 
-	QuadTree_insert(root, (AABB) { .center = { 400.0, 300.0, }, .half_dimension = 42.0f });
-	QuadTree_insert(root, (AABB) { .center = { 200.0, 300.0, }, .half_dimension = 42.0f });
-	QuadTree_insert(root, (AABB) { .center = { 100.0, 300.0, }, .half_dimension = 42.0f });
-	QuadTree_insert(root, (AABB) { .center = { 400.0, 500.0, }, .half_dimension = 42.0f });
+	u32 blocks_size = 0;
+	Block grid[GRID_LENGTH*GRID_LENGTH] = {0};
+	load_grid_by_bin("./levels/level.bin", grid, &blocks_size);
+
+	for (u64 i = 0; i < blocks_size; i++) {
+		QuadTree_insert(root, grid[i]);
+	}
+
+	// QuadTree_insert(root, (Block) { .aabb = { .center = { 400.0, 300.0, }, .half_dimension = 42.0f }, .grid_type = AIR } );
+	// QuadTree_insert(root, (Block) { .aabb = { .center = { 200.0, 300.0, }, .half_dimension = 42.0f }, .grid_type = DESTRUCTIBLE } );
+	// QuadTree_insert(root, (Block) { .aabb = { .center = { 100.0, 300.0, }, .half_dimension = 42.0f }, .grid_type = INDESTRUCTIBLE } );
+	// QuadTree_insert(root, (Block) { .aabb = { .center = { 400.0, 500.0, }, .half_dimension = 42.0f }, .grid_type = INDESTRUCTIBLE } );
 
 	mm_log();
 
