d8801af4ac1ee3a9c73526c1572a8075be44b130
LuanFabricio <luanlimaleite@hotmail.com>
2024-03-08 13:10:41 -0300

    feat: Testing players

--- main.c
+++ main.c
@@ -3,38 +3,39 @@
 #include "quadtree.h"
 #include "./platform.h"
 #include "types.h"
-#include "utils/levels.h"
 #include "levels/level.h"
-#include <stdio.h>
 
 int main()
 {
 	QuadTree *root = QuadTree_new((float)HALF_WIDTH, (float)HALF_HEIGHT, (float)HALF_WIDTH);
 
 	u32 blocks_size = 0;
-	// Block grid[GRID_LENGTH*GRID_LENGTH] = level_bytes;
 	Block *grid = (Block*)level_bytes;
-	// load_grid_by_bin("./levels/level.bin", grid, &blocks_size);
 
-	printf("Size: %u\n", size);
 	for (u64 i = 0; i < size ; i++) {
-		printf("[%llu] {%.2f %.2f} %.2f %u\n", i, grid[i].center.x, grid[i].center.y, GRID_SIZE/2.0, grid[i].grid_type);
 		QuadTree_insert(root, grid[i]);
 	}
 
-
-	// QuadTree_insert(root, (Block) { .aabb = { .center = { 400.0, 300.0, }, .half_dimension = 42.0f }, .grid_type = AIR } );
-	// QuadTree_insert(root, (Block) { .aabb = { .center = { 200.0, 300.0, }, .half_dimension = 42.0f }, .grid_type = DESTRUCTIBLE } );
-	// QuadTree_insert(root, (Block) { .aabb = { .center = { 100.0, 300.0, }, .half_dimension = 42.0f }, .grid_type = INDESTRUCTIBLE } );
-	// QuadTree_insert(root, (Block) { .aabb = { .center = { 400.0, 500.0, }, .half_dimension = 42.0f }, .grid_type = INDESTRUCTIBLE } );
+	Point players[] = {
+		{ GRID_SIZE*4 , GRID_SIZE*4  },
+		{ GRID_SIZE*1 , GRID_SIZE*4  },
+		{ GRID_SIZE*13, GRID_SIZE*13 },
+	};
 
 	mm_log();
 
-	Platform_init_window("Bomb", SCREEN_WIDTH, SCREEN_HEIGHT);
+	Platform_init_window("Bomb royale", SCREEN_WIDTH, SCREEN_HEIGHT);
 
 	while (!Platform_window_should_close()) {
 		Platform_begin_drawing();
 			QuadTree_display(root);
+
+			for (u32 i = 0; i < 3; i++) {
+				Platform_draw_rectangle(
+						players[i].x-(float)GRID_SIZE,
+						players[i].y-(float)GRID_SIZE,
+						GRID_SIZE, GRID_SIZE, 0xff919191);
+			}
 		Platform_end_drawing();
 	}
 
