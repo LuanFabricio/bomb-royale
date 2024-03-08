5e0a899d39ec441412c58ccfbf01e23ad075ac70
LuanFabricio <luanlimaleite@hotmail.com>
2024-03-08 10:50:03 -0300

    fix: Adding sections on platform header

--- platform.h
+++ platform.h
@@ -3,9 +3,13 @@
 
 #include "./types.h"
 
+// ========================== Window section =========================
+
 extern void Platform_init_window(const char* title, u32 w, u32 h);
 extern void Platform_close_window();
 extern boolean Platform_window_should_close();
+
+// ========================== Draw section  ==========================
 
 extern void Platform_begin_drawing();
 extern void Platform_end_drawing();
@@ -14,8 +18,16 @@ extern void Platform_clear_background(u32 color);
 extern void Platform_draw_rectangle(float x, float y, float w, float h, u32 color);
 extern void Platform_draw_rectangle_lines(float x, float y, float w, float h, u32 color);
 
+// ========================== Input section  =========================
+
+extern boolean Platform_is_key_pressed(u8 key);
+
+// ========================== Random section =========================
+
 extern float Platform_rand_seed();
 extern float Platform_rand_float_range(float min, float max);
+
+// ======================== Game loop section ========================
 
 extern void Platform_set_loop(void* loop_fn);
 
