f983e7c7fa9fc4066715726a493fd7a8e3d7b785
LuanFabricio <luanlimaleite@hotmail.com>
2024-04-03 18:20:06 -0300

    feat: Adding max and min function for u8 data type

--- utils/math.h
+++ utils/math.h
@@ -1,6 +1,8 @@
 #ifndef UTILS_H_
 #define UTILS_H_
 
+#include "../types.h"
+
 static float minf(float x, float y)
 {
 	return x > y ? y : x;
@@ -9,6 +11,16 @@ static float minf(float x, float y)
 static float maxf(float x, float y)
 {
 	return x < y ? y : x;
+}
+
+static u8 maxu8(u8 x, u8 y)
+{
+	return x < y ? y : x;
+}
+
+static u8 minu8(u8 x, u8 y)
+{
+	return x < y ? x : y;
 }
 
 #endif // UTILS_H_
