0445c8d6346d7e0436ec9a533449959f3b6de957
LuanFabricio <luanlimaleite@hotmail.com>
2024-04-10 16:22:16 -0300

    fix: Refactor collide variable and fire tick flow
    Removing if condition to set collide flag and expanding if condition for
    fire tick counter

--- utils/fire.h
+++ utils/fire.h
@@ -25,16 +25,13 @@ static void Fire_calc_position(Point* center, u8 idx, u8 axis)
 
 static boolean Fire_handle_collision(FireArray* fires, QuadTree *root, AABB fire_aabb)
 {
-	boolean collide = false;
 	Block *collision_block = QuadTree_check_collision(root, fire_aabb);
-
-	if (collision_block != NULL && collision_block->grid_type != AIR) {
-		collide = true;
-	}
+	boolean collide = collision_block != NULL && collision_block->grid_type != AIR;
 
 	if (collision_block == NULL || collision_block->grid_type == DESTRUCTIBLE) {
 		if (collision_block) {
 			collision_block->grid_type = AIR;
+			QuadTree_remove_by_id(root, collision_block->uid);
 		}
 
 		fires->arr[fires->size].fire_item.center = fire_aabb.center;
@@ -48,8 +45,9 @@ static boolean Fire_handle_collision(FireArray* fires, QuadTree *root, AABB fire
 static u8 Fire_tick(FireArray *fires)
 {
 	for (u8 i = 0; i < fires->size; i++) {
-		if (fires->arr[i].fire_item.tick_to_explode > 0) fires->arr[i].fire_item.tick_to_explode -= 1;
-		else {
+		if (fires->arr[i].fire_item.tick_to_explode > 0) {
+			fires->arr[i].fire_item.tick_to_explode -= 1;
+		} else {
 			fires->arr[i] = fires->arr[fires->size - 1];
 			fires->size -= 1;
 			if (i > 0) i -= 1;
